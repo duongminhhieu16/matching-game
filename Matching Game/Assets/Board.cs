fddb4c300a44697e2f97f1e8bb5d606f1f2cbf74
duongminhhieu16 <duongminhhieu1611@gmail.com>
2020-07-08 15:52:45 +0700

    Game Screen + Pause Screen
    Finishing designing Game Screen and Pause Screen (Pause Screen has 2 button: Resume Button and Main Menu Button) to return to menu.

---
+++ Matching Game/Assets/Board.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int dimension = 10;
+    public List<Sprite> sprites = new List<Sprite>();
+    public float distance = 1.0f;
+    public GameObject tilePrefab;
+    private GameObject[,] allTiles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allTiles = new GameObject[dimension, dimension];
+        SetUp();
+    }
+    private void SetUp()
+    {
+        Vector3 offset = transform.position - new Vector3(dimension * distance / 2.0f, 0);
+        System.Random rand = new System.Random();
+        for (int i = 0; i < dimension; i++)
+        {
+            for (int j = 0; j < dimension; j++)
+            {
+                GameObject newTile = Instantiate(tilePrefab);
+                SpriteRenderer renderer = newTile.GetComponent<SpriteRenderer>();
+                
+                int num = rand.Next(5);
+                Debug.Log(num);
+                renderer.sprite = sprites[num];
+                newTile.transform.parent = transform;
+                newTile.transform.position = new Vector3(i * distance, j * distance, 0) + offset;
+                allTiles[i, j] = newTile;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Matching Game/Assets/Pause.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    bool isPaused = false;
+    
+    public void pauseGame()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 1;
+            isPaused = false;
+        }
+        else
+        {
+            Time.timeScale = 0;
+            isPaused = true;
+        }
+    }
+
+    public void backToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
